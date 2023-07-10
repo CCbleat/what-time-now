12c9d2b59e09978caa1caafa715b26769b8aeb38
ccbleat <ccxiaoy@outlook.com>
2023-07-10 14:14:11 +0800

    feat: basic functions of an online clock website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // create realting variables
+  let [hour, setHour] = useState(0);
+  let [minute, setMinute] = useState(0);
+  let [second, setSecond] = useState(0);
+  
+  function updateTime() {
+    const time = new Date();
+    setHour(time.getHours());
+    setMinute(time.getMinutes());
+    setSecond(time.getSeconds());
+  }
+  
+  useEffect(() => {
+    updateTime();
+    setInterval(updateTime, 1000);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Time-container'>
+        <div className='Hour-container Time-page'>{hour}</div>
+        <div className='Minute-container Time-page'>{minute}</div>
+        <div className='Second-container Time-page'>{second}</div>
+      </div>
     </div>
   );
 }
